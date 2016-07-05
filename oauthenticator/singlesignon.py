eca7bc64fc1b584b1d56388d910a3397bb1fd1bf
Charles Flores <cflores@stratio.com>
2016-07-05 08:28:23 +0200

    Login agains SSO sevice

--- oauthenticator/singlesignon.py
+++ oauthenticator/singlesignon.py
@@ -27,10 +27,10 @@ if GITHUB_HOST == 'github.com':
 else:
     GITHUB_API = '%s/api/v3/user' % GITHUB_HOST
 
-AUX_OAUTH_AUTHORIZE_URL = "https://gosec.int.stratio.com:443/cas/oauth2.0/authorize"
-# "https://%s/login/oauth/authorize" % GITHUB_HOST
-AUX_OAUTH_ACCESS_TOKEN_URL = "https://gosec.int.stratio.com:443/gosec-sso/oauth2.0/accessToken"
-# "https://%s/login/oauth/access_token" % GITHUB_HOST
+AUX_OAUTH_AUTHORIZE_URL = "https://gosec.int.stratio.com/gosec-sso/oauth2.0/authorize"
+# AUX_OAUTH_AUTHORIZE_URL = "https://%s/login/oauth/authorize" % GITHUB_HOST
+AUX_OAUTH_ACCESS_TOKEN_URL = "https://gosec.int.stratio.com/gosec-sso/oauth2.0/accessToken"
+# AUX_OAUTH_ACCESS_TOKEN_URL = "https://%s/login/oauth/access_token" % GITHUB_HOST
 
 
 class SingleSignOnMixin(OAuth2Mixin):
@@ -60,40 +60,49 @@ class SingleSignOnOAuthenticator(OAuthenticator):
         # See: https://developer.github.com/v3/oauth/
 
         # GitHub specifies a POST request yet requires URL parameters
+        aux_grant_type = "authorization_code"
         params = dict(
             client_id=self.client_id,
             client_secret=self.client_secret,
+            grant_type=aux_grant_type,
+            redirect_uri="https://intelligence-dev:8000/hub/oauth_callback",
             code=code
         )
 
 
-        url = _OAUTH_ACCESS_TOKEN_URL
-        req = HTTPRequest(url,
-                          method="POST",
-                          headers={"Accept": "application/json"},
-                          body='' # Body is required for a POST...
-                          )
+        url = os.getenv('OAUTH_ACCESS_TOKEN_URL', AUX_OAUTH_ACCESS_TOKEN_URL) # _OAUTH_ACCESS_TOKEN_URL
+        self.log.info('AUX_OAUTH_ACCESS_TOKEN_URL -- ' + url)
 
-        resp = yield http_client.fetch(req)
-        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-
-        access_token = resp_json['details']['tokenValue']
-
-        # Determine who the logged in user is
-        headers={"Accept": "application/json",
-                 "User-Agent": "JupyterHub",
-                 "Authorization": "token {}".format(access_token)
-        }
-        req = HTTPRequest("https://%s" % GITHUB_API,
+        ######
+        url = url_concat(url, params)
+        ######
+        self.log.info('AUX_OAUTH_ACCESS_TOKEN_URL CONCATENATED -- ' + url)
+        req = HTTPRequest(url=url,
                           method="GET",
-                          headers=headers
+                          headers={"Accept": "application/json"}
                           )
-        resp = yield http_client.fetch(req)
-        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-        # TODO Retrieve userprofile that should return user's attributes
-        self.log.warn("---authenticate---")
 
-        return resp_json["userAuthentication"]["details"]["id"]
+        resp = yield http_client.fetch(req)
+
+        body = resp.body.decode('utf8', 'replace')
+        body = body.replace("=", "\":\"")
+        body = body.replace("&", "\",\"")
+        json_str = "{\""+body+"\"}"
+        print ("print json_str -> " + json_str)
+        resp_json = json.loads(json_str)
+        print (body)
+        access_token = resp_json['access_token'] #resp_json['details']['tokenValue']
+        aux_url = "https://gosec.int.stratio.com/gosec-sso/oauth2.0/profile?access_token=%s" % access_token
+        print (aux_url)
+        req = HTTPRequest(aux_url)
+        resp = yield http_client.fetch(req)
+        # body_req2 = resp.body.decode('utf8', 'replace')
+        resp_json2 = json.loads(resp.body.decode('utf8', 'replace'))
+        # TODO Retrieve userprofile that should return user's attributes
+        self.log.warn("---authenticate---: " + resp_json2['id'])
+        # self.log.info(body_req2)
+
+        return resp_json2['id']
 
     # self.log.warn("---SingleSignOnOAuthenticator---")
 
