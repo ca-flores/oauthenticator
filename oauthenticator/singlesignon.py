e980c7dcf8fca842bfd3a5a7f0cdbc75afa94fc9
Charles Flores <cflores@stratio.com>
2016-07-06 10:52:31 +0200

    Code reviewed and reordered

--- oauthenticator/singlesignon.py
+++ oauthenticator/singlesignon.py
@@ -22,10 +22,11 @@ from traitlets import Unicode
 
 from .oauth2 import OAuthLoginHandler, OAuthenticator
 
+# Looking for urls that have been set as Environment Variables
 AUX_OAUTH_AUTHORIZE_URL = "https://gosec.int.stratio.com/gosec-sso/oauth2.0/authorize"
-# AUX_OAUTH_AUTHORIZE_URL = "https://%s/login/oauth/authorize" % GITHUB_HOST
 AUX_OAUTH_ACCESS_TOKEN_URL = "https://gosec.int.stratio.com/gosec-sso/oauth2.0/accessToken"
-# AUX_OAUTH_ACCESS_TOKEN_URL = "https://%s/login/oauth/access_token" % GITHUB_HOST
+AUX_OAUTH_PROFILE_URL = "https://gosec.int.stratio.com/gosec-sso/oauth2.0/profile"
+AUX_OAUTH_LOGOUT_URL = "https://gosec.int.stratio.com/gosec-sso/logout"
 
 
 class SingleSignOnMixin(OAuth2Mixin):
@@ -35,28 +36,18 @@ class SingleSignOnMixin(OAuth2Mixin):
 class SingleSignOnLoginHandler(OAuthLoginHandler, SingleSignOnMixin):
     pass
 
-class SingleSignOnLogoutHandler(BaseHandler):
+class SingleSignOnLogoutHandler(BaseHandler, OAuthenticator):
     """Class for OAuth logout handler"""
-    oauth_logout_url = Unicode(
-        os.getenv('OAUTH_LOGOUT_URL', 'https://gosec.int.stratio.com/gosec-sso/logout'),
-        config=True,
-        help="""Logout URL to use.
-        Typically `https://{host}/hub/logout
-        `"""
-    )
+
     @gen.coroutine
     def get(self):
-        self.log.info("SingleSignOnLogoutHandler----")
-
-        self.log.debug("oauth_logout to be called----")
         http_client = AsyncHTTPClient()
-        req =  HTTPRequest(url='https://gosec.int.stratio.com/gosec-sso/logout',
+        req =  HTTPRequest(url=self.oauth_logout_url,
                           method="GET",
                           headers={"Accept": "application/json"}
                           )
         resp = yield http_client.fetch(req)
         decoded = resp.body.decode()
-        # self.log.info(decoded)
         user = self.get_current_user()
         if user:
             self.log.info("User logged out: %s", user.name)
@@ -64,7 +55,6 @@ class SingleSignOnLogoutHandler(BaseHandler):
             for name in user.other_user_cookies:
                 self.clear_login_cookie(name)
             user.other_user_cookies = set([])
-            # self.statsd.incr('logout')
         self.redirect(self.hub.server.base_url, permanent=False)
 
 
@@ -77,7 +67,7 @@ class SingleSignOnOAuthenticator(OAuthenticator):
     login_handler = SingleSignOnLoginHandler
     logout_handler = SingleSignOnLogoutHandler
     oauth_logout_url = Unicode(
-        os.getenv('OAUTH_LOGOUT_URL', 'https://gosec.int.stratio.com/gosec-sso/logout'),
+        os.getenv('OAUTH_LOGOUT_URL', AUX_OAUTH_LOGOUT_URL),
         config=True,
         help="""Logout URL to use.
         Typically `https://{host}/hub/logout
@@ -86,21 +76,15 @@ class SingleSignOnOAuthenticator(OAuthenticator):
     grant_type = Unicode(config=True)
     @gen.coroutine
     def authenticate(self, handler, data=None):
-        self.log.warn("...authenticate...")
+        self.log.info("SingleSignOnOAuthenticator...authenticate...")
         code = handler.get_argument("code", False)
         if not code:
             raise web.HTTPError(400, "oauth callback made without a token")
         # TODO: Configure the curl_httpclient for tornado
         http_client = AsyncHTTPClient()
 
-        # Exchange the OAuth code for a GitHub Access Token
-        #
-        # See: https://developer.github.com/v3/oauth/
-
-        # GitHub specifies a POST request yet requires URL parameters
-
-        # aux_grant_type = "authorization_code"
-        # "https://intelligence-dev:8000/hub/oauth_callback",
+        # Exchange the OAuth code for a Custom OAuth Access Token
+        # API specifies a GET request yet requires URL parameters
         params = dict(
             client_id=self.client_id,
             client_secret=self.client_secret,
@@ -109,42 +93,41 @@ class SingleSignOnOAuthenticator(OAuthenticator):
             code=code
         )
 
+        url = os.getenv('OAUTH_ACCESS_TOKEN_URL', '')
+        if not url:
+            raise web.HTTPError(400, "OAUTH_ACCESS_TOKEN_URL is not defined")
 
-        url = os.getenv('OAUTH_ACCESS_TOKEN_URL', AUX_OAUTH_ACCESS_TOKEN_URL) # _OAUTH_ACCESS_TOKEN_URL
-        self.log.info('AUX_OAUTH_ACCESS_TOKEN_URL -- ' + url)
-
-        ######
         url = url_concat(url, params)
-        ######
-        self.log.info('AUX_OAUTH_ACCESS_TOKEN_URL CONCATENATED -- ' + url)
         req = HTTPRequest(url=url,
                           method="GET",
                           headers={"Accept": "application/json"}
                           )
 
         resp = yield http_client.fetch(req)
-
-        body = resp.body.decode('utf8', 'replace')
-        body = body.replace("=", "\":\"")
-        body = body.replace("&", "\",\"")
+        # Parse fetch response, in this case fetch returns a string
+        # that is processed to convert it to JSON string format
+        # We retrieve a valid token and an extra param that is expired
+        body_str = resp.body.decode('utf8', 'replace')
+        body_str = body.replace("=", "\":\"")
+        body_str = body.replace("&", "\",\"")
         json_str = "{\""+body+"\"}"
-        print ("print json_str -> " + json_str)
+        self.log.debug("Response valid token: %s" % json_str)
+        # Parse json_str to json object
         resp_json = json.loads(json_str)
-        print (body)
-        access_token = resp_json['access_token'] #resp_json['details']['tokenValue']
-        aux_url = "https://gosec.int.stratio.com/gosec-sso/oauth2.0/profile?access_token=%s" % access_token
-        print (aux_url)
+
+        params = yield dict(
+            access_token=resp_json['access_token']
+        )
+        # Retrieve user information with a valid token obtained from the previous
+        # request
+        url_profile = url_concat(AUX_OAUTH_PROFILE_URL, params)
         req = HTTPRequest(aux_url)
         resp = yield http_client.fetch(req)
-        # body_req2 = resp.body.decode('utf8', 'replace')
-        resp_json2 = json.loads(resp.body.decode('utf8', 'replace'))
-        # TODO Retrieve userprofile that should return user's attributes
-        self.log.warn("---authenticate---: " + resp_json2['id'])
-        # self.log.info(body_req2)
-
-        return resp_json2['id']
-
-    # self.log.warn("---SingleSignOnOAuthenticator---")
+        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
+        # This request returns a JSON string
+        self.log.info("OAuth user id: %s" % resp_json['id'])
+        # User id is returned to be registered into app data base
+        return resp_json['id']
 
 
 class LocalSingleSignOnOAuthenticator(LocalAuthenticator, SingleSignOnOAuthenticator):
