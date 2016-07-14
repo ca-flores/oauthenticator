0fe4b335edc241437b45f5ca72b4e75dba712753
ca-flores <cflores@stratio.com>
2016-07-14 09:19:52 +0100

    Stratio Single Sign On (#1)
    * first version of singlesignon
    
    * first version, authentication in gosec
    
    * Login agains SSO sevice
    
    * Logout Oauth implemented
    
    * Code reviewed and reordered
    
    * config properties added
    
    * Fixed logout from notebook app
    
    * Externalize oauth urls
    
    * avoiding to access to environment variables
    
    * Parser functions added and custom logout relocated into custom logout class

--- oauthenticator/oauth2.py
+++ oauthenticator/oauth2.py
@@ -6,6 +6,7 @@ Most of the code c/o Kyle Kelley (@rgbkrk)
 
 
 import os
+import subprocess
 
 from tornado import gen, web
 
@@ -13,12 +14,14 @@ from jupyterhub.handlers import BaseHandler
 from jupyterhub.auth import Authenticator
 from jupyterhub.utils import url_path_join
 
+from tornado.httpclient import AsyncHTTPClient
+
 from traitlets import Unicode
 
 
 class OAuthLoginHandler(BaseHandler):
     """Base class for OAuth login handler
-    
+
     Typically subclasses will need
     """
     scope = []
@@ -32,16 +35,22 @@ class OAuthLoginHandler(BaseHandler):
                 'oauth_callback'
             )
         )
-        
+
         redirect_uri = self.authenticator.oauth_callback_url or guess_uri
         self.log.info('oauth redirect: %r', redirect_uri)
-        
+
+        self.set_oauth_urls(
+            oauth_authorize_url=self.authenticator.oauth_authorize_url,
+            oauth_access_token_url=self.authenticator.oauth_access_token_url)
+
         self.authorize_redirect(
             redirect_uri=redirect_uri,
             client_id=self.authenticator.client_id,
             scope=self.scope,
             response_type='code')
 
+class OAuthLogoutHandler(BaseHandler):
+    pass
 
 class OAuthCallbackHandler(BaseHandler):
     """Basic handler for OAuth callback. Calls authenticator to verify username."""
@@ -61,44 +70,70 @@ class OAuthCallbackHandler(BaseHandler):
 
 class OAuthenticator(Authenticator):
     """Base class for OAuthenticators
-    
+
     Subclasses must override:
-    
+
     login_service (string identifying the service provider)
     login_handler (likely a subclass of OAuthLoginHandler)
     authenticate (method takes one arg - the request handler handling the oauth callback)
     """
-    
+
     login_service = 'override in subclass'
+    
+    oauth_authorize_url = Unicode(config=True)
+    oauth_access_token_url = Unicode(config=True)
     oauth_callback_url = Unicode(
-        os.getenv('OAUTH_CALLBACK_URL', ''),
         config=True,
         help="""Callback URL to use.
         Typically `https://{host}/hub/oauth_callback`"""
     )
-    
+
+    oauth_logout_url = Unicode(
+        config=True,
+        help="""Logout URL to use.
+        Typically `https://{host}/hub/logout
+        `"""
+    )
+    oauth_profile_url = Unicode(config=True)
+    grant_type = Unicode(config=True)
+
+
     client_id_env = 'OAUTH_CLIENT_ID'
     client_id = Unicode(config=True)
     def _client_id_default(self):
         return os.getenv(self.client_id_env, '')
-    
+
     client_secret_env = 'OAUTH_CLIENT_SECRET'
     client_secret = Unicode(config=True)
     def _client_secret_default(self):
         return os.getenv(self.client_secret_env, '')
-    
+
     def login_url(self, base_url):
         return url_path_join(base_url, 'oauth_login')
-    
+
+    def logout_url(self, base_url):
+        return url_path_join(base_url, 'logout')
+
     login_handler = "Specify login handler class in subclass"
+    logout_handler = "Specify logout handler class in subclass"
     callback_handler = OAuthCallbackHandler
-    
+
     def get_handlers(self, app):
         return [
             (r'/oauth_login', self.login_handler),
+            (r"/logout", self.logout_handler),
             (r'/oauth_callback', self.callback_handler),
         ]
+
     
+    def parse_response(self, response):
+        """ 
+        Input: response of oauth service. It contains a valid token 
+        Ouput: parsed response in json or any other valid format 
+            for the current authenticator
+        """
+        raise NotImplementedError()
+
     @gen.coroutine
     def authenticate(self, handler, data=None):
         raise NotImplementedError()
---
+++ oauthenticator/singlesignon.py
@@ -0,0 +1,128 @@
+"""
+Custom Authenticator to set an own Single Sign On server with JupyterHub
+
+Most of the code c/o Kyle Kelley (@rgbkrk)
+"""
+
+
+import json
+import os
+
+from tornado.concurrent import return_future
+from tornado.auth import OAuth2Mixin
+from tornado import gen, web
+
+from tornado.httputil import url_concat
+from tornado.httpclient import HTTPRequest, AsyncHTTPClient
+
+from jupyterhub.utils import url_path_join
+from jupyterhub.auth import LocalAuthenticator
+from jupyterhub.handlers import BaseHandler
+
+from traitlets import Unicode
+
+from .oauth2 import OAuthLoginHandler, OAuthLogoutHandler, OAuthenticator
+
+
+class SingleSignOnMixin(OAuth2Mixin):
+    
+    def set_oauth_urls(self, oauth_authorize_url = None, oauth_access_token_url=None):
+        self._OAUTH_AUTHORIZE_URL = oauth_authorize_url
+        self._OAUTH_ACCESS_TOKEN_URL = oauth_access_token_url
+
+class SingleSignOnLoginHandler(OAuthLoginHandler, SingleSignOnMixin):
+    pass
+
+class SingleSignOnLogoutHandler(OAuthLogoutHandler):
+    
+    """ Class for OAuth logout handler """
+    @gen.coroutine
+    def get(self):
+        http_client = AsyncHTTPClient()
+        user = self.get_current_user()
+        if user:
+            self.log.info("User logged out: %s", user.name)
+            self.clear_login_cookie()
+            for name in user.other_user_cookies:
+                self.clear_login_cookie(name)
+            user.other_user_cookies = set([])
+        # Stratio Intelligence Modification
+        # Stop_single_user added in Sprint 7 in order to stop container in logout
+        self.stop_single_user(user)
+        self.redirect(self.authenticator.oauth_logout_url, permanent=False)
+
+class SingleSignOnOAuthenticator(OAuthenticator):
+    """
+    Custom Authenticator for custom OAuth server
+    """
+    login_service = "SingleSignOn"
+    client_secret_env = 'SINGLESIGNON_CLIENT_SECRET'
+    login_handler = SingleSignOnLoginHandler
+    logout_handler = SingleSignOnLogoutHandler
+
+
+    def parse_response(self, response):
+        # Parse fetch response, in this case fetch returns a string
+        # that is processed to convert it to JSON string format
+        # We retrieve a valid token and an extra param that is called "expired"
+        body_str = response.body.decode('utf8', 'replace')
+        body_str = body_str.replace("=", "\":\"")
+        body_str = body_str.replace("&", "\",\"")
+        json_str = "{\""+body_str+"\"}"
+        self.log.debug("Response valid token: %s" % json_str)
+        # Parse json_str to json object
+        return json.loads(json_str)
+
+    @gen.coroutine
+    def authenticate(self, handler, data=None):
+
+        code = handler.get_argument("code", False)
+        if not code:
+            raise web.HTTPError(400, "OAUTH_CALLBACK_URL has been called without a token")
+
+        http_client = AsyncHTTPClient()
+
+        # Exchange the OAuth code for a Custom OAuth Access Token
+        # API specifies a GET request yet requires URL parameters
+        token_req_param = dict(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            grant_type=self.grant_type,
+            redirect_uri=self.oauth_callback_url,
+            code=code
+        )
+
+        if not self.oauth_access_token_url:
+            raise web.HTTPError(400, "OAUTH_ACCESS_TOKEN_URL is not defined")
+
+        token_url = url_concat(self.oauth_access_token_url, token_req_param)
+        token_req = HTTPRequest(url=token_url,
+                          method="GET",
+                          headers={"Accept": "application/json"}
+                          )
+
+        resp = yield http_client.fetch(token_req)
+        
+        parse_response = self.parse_response(resp)
+
+        profile_req_params = dict(
+            access_token=resp_json['access_token']
+        )
+        # Retrieve user information with a valid token obtained from the previous
+        # request
+        if not self.oauth_profile_url:
+            raise web.HTTPError(400, "OAUTH_PROFILE_URL is not defined")
+        profile_url = url_concat(self.oauth_profile_url, profile_req_params)
+        profile_req = HTTPRequest(profile_url)
+        resp = yield http_client.fetch(profile_req)
+        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
+        # This request returns a JSON string
+        self.log.info("OAuth user id: %s" % resp_json['id'])
+        # User id is returned to be registered into app data base
+        return resp_json['id']
+
+
+class LocalSingleSignOnOAuthenticator(LocalAuthenticator, SingleSignOnOAuthenticator):
+
+    """A version that mixes in local system user creation"""
+    pass
